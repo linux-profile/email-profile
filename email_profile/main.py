b75d4ebe651b34d46e97423e5d3f86b4adac38db
FernandoCelmer <email@fernandocelmer.com>
2024-04-25 23:59:35 -0300

    ⚙️ FEATURE: Email

--- email_profile/main.py
+++ email_profile/main.py
@@ -1,5 +1,25 @@
+import os
+import email
+
+from dotenv import load_dotenv
+from core import Email
+
+
 def main():
-    pass
+    """Test"""
+    load_dotenv()
+
+    app = Email(
+        server=os.getenv("EMAIL_SERVER"),
+        username=os.getenv("EMAIL_USERNAME"),
+        password=os.getenv("EMAIL_PASSWORD")
+    )
+
+    app.login()
+    data = app.backup(10)
+
+    for item in data.email:
+        print(item.number, item.subject)
 
 
 if __name__ == '__main__':
