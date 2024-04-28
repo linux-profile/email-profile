2066bf6ee4b9c5bee7b566ab723fc2db841a26ca
FernandoCelmer <email@fernandocelmer.com>
2024-04-28 19:13:12 -0300

    📦 PyPI-Test - Build 0.2.0-dev.6

--- email_profile/__init__.py
+++ email_profile/__init__.py
@@ -23,6 +23,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE."""
 
-from email_profile.core import Email
+# from email_profile.core import Email
 
-__all__ = ['Email']
+# __all__ = ['Email']
