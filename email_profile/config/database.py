cf4a23b0ae336bb31cd5d60721f3771496438f5e
FernandoCelmer <email@fernandocelmer.com>
2024-04-28 23:40:14 -0300

    ⚙️ FEATURE: Change connection structure to the Database

--- email_profile/config/database.py
+++ email_profile/config/database.py
@@ -2,21 +2,7 @@
 Database Module
 """
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, registry
+import sqlite3
 
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-mapper_registry = registry()
-Base = mapper_registry.generate_base()
+engine = sqlite3.connect("sql_app.db")
+session = engine.cursor()
