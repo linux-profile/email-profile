8d15cebb7ec3060479339fce7de9629e64a352ff
Fernando Celmer <email@fernandocelmer.com>
2026-04-15 19:32:23 -0300

    🪲 BUG-#6: Drop legacy email_profile modules and obsolete root files

--- email_profile/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from email_profile.models.attachment import AttachmentModel
-from email_profile.models.email import EmailModel
-from email_profile.models.mailbox import MailBoxModel
-
-__all__ = [
-    "AttachmentModel",
-    "EmailModel",
-    "MailBoxModel",
-]
